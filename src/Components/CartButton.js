622d365df10f75b89992cbc113a79702ed677db9
Zviad Mikeladze <zviadipf@gmail.com>
2022-08-09 15:07:56 +0400

    Final 09.08

--- src/Components/CartButton.js
+++ src/Components/CartButton.js
@@ -1,16 +1,34 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartContext from "../store/cart-context";
 import CartIcon from "./CartIcon";
 
 import classes from "./HeaderCartButton.module.css";
 
 const CartButton = (props) => {
+  const [btnIs, setBtnIs] = useState(false);
   const cartCtx = useContext(CartContext);
-  const badgeNumber = cartCtx.items.reduce((curNumb , item)=>{
-    return curNumb +item.amount
-  },0);
+  const badgeNumber = cartCtx.items.reduce((curNumb, item) => {
+    return curNumb + item.amount;
+  }, 0);
+  const { items } = cartCtx;
+  const btnClasses = `${classes.button} ${btnIs ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIs(true);
+
+    const timer = setTimeout(() => {
+      setBtnIs(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
--- src/Components/CartItem.js
+++ src/Components/CartItem.js
@@ -5,11 +5,11 @@ const CartItem = (props) => {
 
   return (
     <li className={classes["cart-item"]}>
-      <div>
+      <div >
         <h2>{props.name}</h2>
         <div className={classes.summary}>
           <span className={classes.price}>{price}</span>
-          <span className={classes.amount}>{props.amount}</span>
+          <span className={classes.amount}>x {props.amount}</span>
         </div>
       </div>
       <div className={classes.actions}>
